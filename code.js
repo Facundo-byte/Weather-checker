7ed958fe6a4b801b0a8cc58b3edd82d50cbbadbf
Facundo-byte <facundodavidiascas@gmail.com>
2025-05-18 21:02:39 -0300

    Actualizacion 1.0

--- code.js
+++ code.js
@@ -1,8 +1,8 @@
-
 const ul = document.querySelector(".ciudades");
 const enviar = document.getElementById("boton");
 const texto = document.getElementById("texto");
 const texterr = document.getElementById("error");
+
 let ciudadesingresadas = [];
 
 enviar.addEventListener("click",function(){
@@ -10,23 +10,25 @@ enviar.addEventListener("click",function(){
 })
 
 async function fetchPrincipal(){
-    const apikey = "9c2825d2ca070bd59cb3e939c8da349a";
+    const apikey = ""; // <--------- Use your API key from OpenWeather HERE
 
     const li = document.createElement("li");
     li.classList.add("elemento");
 
     const textoingresado = texto.value;
     texterr.innerHTML = "";
+    texterr.style.display = "none";
 
     try{ 
         const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${textoingresado}&units=metric&appid=${apikey}`); 
 
         if(!response.ok){ 
-            throw new Error("Error encontrado"); 
+            throw new Error("Error found"); 
         }
 
         if(ciudadesingresadas.includes(textoingresado)){
-            texterr.innerHTML = "Ya escribiste esa ciudad";
+            texterr.style.display = "block";
+            texterr.innerHTML = `You already know the weather of ${textoingresado}`;
             return;
         }
 
@@ -35,7 +37,7 @@ async function fetchPrincipal(){
         const datos=`
         <p class="ciudad">${data.name} <span class="pais">${data.sys.country}</span></p>
         <h1 class="temp">${parseInt(data.main.temp) }°C</h1>
-        <img class="imagen" src="https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" alt="no hay imagen xd">
+        <img class="imagen" src="https://openweathermap.org/img/wn/${data.weather[0].icon}@4x.png" alt="no hay imagen xd">
         <p class="descripcion">${data.weather[0].description.toUpperCase()}</p>`
 
         console.log(textoingresado);
@@ -49,20 +51,8 @@ async function fetchPrincipal(){
 
     catch(error){ //en caso de que falle, con el catch atrapamos el error y con el console lo printeamos en la consola
         console.error(error);
-        texterr.innerHTML = "escribi un nombre valido";
+        texterr.style.display = "block";
+        texterr.innerHTML = "Write a valid name";
     }
 }
 
-/*
-function Funcion(){
-    const li = document.createElement("li");
-    const datos=`<h2>Athens</h2>
-    <p class="xd">pais</p>
-    <h1>11 °c</h1>
-    <img src="" alt="no hay imagen xd">
-    <p>descripcion</p>`
-
-    li.classList.add("elemento");
-    li.innerHTML = datos;
-    ul.appendChild(li);
-}*/
